dad998379cdd194311f94116a1defee0a546630c
Seth Jetro <jsethjetro@gmail.com>
2018-04-10 10:59:05 -0500

    Generate QrCode with basic information, more imrove coming

--- app/src/main/java/com/mobil/pronap/cash0/Activities/DetailSellActivity.java
+++ app/src/main/java/com/mobil/pronap/cash0/Activities/DetailSellActivity.java
@@ -1,6 +1,7 @@
 package com.mobil.pronap.cash0.Activities;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -9,6 +10,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 import com.mobil.pronap.cash0.R;
 
 public class DetailSellActivity extends AppCompatActivity {
@@ -30,8 +36,19 @@ public class DetailSellActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 //generate QR code
-                i = new Intent(DetailSellActivity.this, QRViewerActivity.class);
-                startActivity(i);
+                MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+                try{
+                    BitMatrix bitMatrix = multiFormatWriter.encode(etProductName.getText().toString(), BarcodeFormat.QR_CODE, 300,300);
+                    BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+                    Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+
+                    i = new Intent(DetailSellActivity.this, QRViewerActivity.class);
+                    i.putExtra("qrCode", bitmap);
+                    startActivity(i);
+                }
+                catch (WriterException e){
+                    e.printStackTrace();
+                }
             }
         });
 
@@ -40,11 +57,11 @@ public class DetailSellActivity extends AppCompatActivity {
 
 
     public void init_views(){
-        etProductName = (EditText) findViewById(R.id.etProductName);
-        etProductDetail = (EditText) findViewById(R.id.etProductDetail);
-        etProductPrice = (EditText) findViewById(R.id.etProductPrice);
-        btnGenerateQR = (Button) findViewById(R.id.btnGenarateQR);
-        customToolbar = (Toolbar) findViewById(R.id.customToolbar);
+        etProductName =  findViewById(R.id.etProductName);
+        etProductDetail = findViewById(R.id.etProductDetail);
+        etProductPrice = findViewById(R.id.etProductPrice);
+        btnGenerateQR = findViewById(R.id.btnGenarateQR);
+        customToolbar = findViewById(R.id.customToolbar);
         setSupportActionBar(customToolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
