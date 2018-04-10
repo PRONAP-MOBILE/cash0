d83d1f326a15643d4cb8493ca3143259320c63cc
pbobc10 <pbobc10@gmail.com>
2018-04-10 10:29:24 -0500

    model1

---
+++ app/src/main/java/com/mobil/pronap/cash0/models/User.java
@@ -0,0 +1,85 @@
+package com.mobil.pronap.cash0.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class User {
+
+    private String nif;
+    private String phone;
+    private String email;
+    private String userName;
+    private int cardNumber;
+
+    public String getNif() {
+        return nif;
+    }
+
+    public void setNif(String nif) {
+        this.nif = nif;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(int cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public User(JSONObject jsonObject) {
+        try {
+            this.nif = jsonObject.getString("nif");
+            this.phone = jsonObject.getString("phone");
+            this.email = jsonObject.getString("email");
+            this.userName = jsonObject.getString("userName");
+            this.cardNumber = jsonObject.getInt("cardNumber");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public static ArrayList<User> fromJson(JSONArray jsonArray){
+        ArrayList<User> users = new ArrayList<User>();
+        for (int i=0; i< jsonArray.length();i++){
+
+            try {
+                users.add(new User(jsonArray.getJSONObject(i)));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return  users;
+    }
+}
+
